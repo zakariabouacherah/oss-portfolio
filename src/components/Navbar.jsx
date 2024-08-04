4cf833d59c5e0fb305aae8cacb7617401a54b165
Bouach <zakabouach111@gmail.com>
2024-08-04 13:20:08 +0100

    new version hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const MENU_ITEMS = [
 ];
 
 const menuItemStyle =
-  "transition hover:text-[#b9090b] border-b-[1.5px] border-b-transparent hover:border-b-[#b9090b] cursor-pointer";
+  "transition hover:text-[#ff9a00] border-b-[1.5px] border-b-transparent hover:border-b-[#ff9a00] cursor-pointer";
 
 const Navbar = () => {
   const [color, setColor] = useState(false);
@@ -52,24 +52,38 @@ const Navbar = () => {
         color ? "backdrop-blur-md bg-[#44444471] shadow-md" : "bg-transparent"
       }`}
     >
-      <a
+      {/* <a
         href="#home"
-        className="font-bold text-2xl text-[#b9090b] cursor-pointer"
+        className="font-bold text-2xl text-[#ff9a00] cursor-pointer"
       >
         OI
+      </a> */}
+      <a
+        onClick={closeNavbar}
+        href="#contact"
+        className="w-10 h-10 bg-gradient-to-b from-[#e8c365bd] to-[#b9090c3f] shadow-md rounded-full flex items-center justify-center"
+      >
+        <img
+          src={logo}
+          alt="avatar"
+          className="w-10 h-10 object-contain rounded-full"
+        />
       </a>
       <div
-        className={`flex absolute lg:relative  left-0 w-screen lg:w-full h-screen lg:h-full  bg-black lg:bg-transparent flex-col-reverse justify-end pt-14 lg:pt-0 lg:justify-between lg:top-0 lg:flex-row items-center gap-14 transition-all duration-300 ease-in ${
+        className={`flex absolute lg:relative  left-0 w-screen lg:w-full h-screen lg:h-full  bg-black lg:bg-transparent flex-col-reverse justify-end pt-14 lg:pt-0 lg:justify-around lg:top-0 lg:flex-row-reverse items-center gap-14 transition-all duration-300 ease-in ${
           isOpen ? "top-0 " : "top-[-1000px] "
         }`}
       >
+        <div className="py-1 px-3 border-[#ff9a00] border-[1px] rounded-full hover:bg-[#ff9a00] hover:text-white transition cursor-pointer">
+          Download CV
+        </div>
         <ul className={`items-center gap-6 flex flex-col lg:flex-row `}>
           {MENU_ITEMS.map((item) => (
             <a key={item.id} href={`/#${item.id}`}>
               <li
                 className={`${menuItemStyle} ${
                   activeMenuItem === item.id
-                    ? "border-b-[#b9090b] text-[#b9090b]"
+                    ? "border-b-[#ff9a00] text-[#ff9a00]"
                     : ""
                 }`}
                 onClick={closeNavbar}
@@ -80,17 +94,6 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <a
-          onClick={closeNavbar}
-          href="#contact"
-          className="w-10 h-10 bg-gradient-to-b from-[#e8c365bd] to-[#b9090c3f] shadow-md rounded-full flex items-center justify-center"
-        >
-          <img
-            src={logo}
-            alt="avatar"
-            className="w-10 h-10 object-contain rounded-full"
-          />
-        </a>
         <div
           className="flex lg:hidden text-2xl cursor-pointer"
           onClick={closeNavbar}
--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -1,29 +1,49 @@
-const HeroSection = () => {
-  const bg = { backgroundImage: 'url("../public/1.png")' };
+import img from "../assets/01-01.png";
 
+const HeroSection = () => {
   return (
     <div
       id="home"
       // style={bg}
-      className=" h-screen bg-contain md:bg-cover bg-no-repeat bg-center md:bg-bottom overflow-hidden flex flex-col justify-center px-6 md:px-24 lg:px-32 bg-[#000]"
+      className=" h-screen bg-contain md:bg-cover bg-no-repeat bg-center md:bg-bottom overflow-hidden flex flex-col lg:flex-row justify-center lg:justify-between items-center px-6 md:px-24 lg:px-32 bg-[#0e0e0e] w-full relative"
     >
-      <div className=" flex flex-col  text-white">
+      <div className=" flex flex-col gap-6  text-white lg:w-[50%]">
         <div className="font-semibold opacity-20 text-2xl md:text-4xl">
-          I'm a
+          Hello !
         </div>
-        <h1 className="text-6xl md:text-9xl font-bold">
-          <div>GRAPHIC</div>
+        <h1 className="text-6xl md:text-8xl font-bold">
           <div>
-            DESIGNER<span className="text-[#b9090b]">.</span>
+            I'm <span className="text-[#ff9a00]">Oussama</span>
           </div>
+          <div className="text-[#ff9a00]">ICHID</div>
         </h1>
-        <a
-          href="/#contact"
-          className="bg-[#b9090b] text-white place-self-start font-semibold rounded-md text-md md:text-md p-2 tracking-wide shadow-md"
-        >
-          CONTACT ME
-        </a>
+        <div className="font-semibold text-lg">
+          Graphic Designer / Photographer
+        </div>
+        <div className="flex items-center gap-6">
+          <a
+            href="/#contact"
+            className="bg-[#ff9a00] text-white place-self-start font-semibold rounded-full text-md md:text-md py-2 px-4 tracking-wide shadow-md"
+          >
+            CONTACT ME
+          </a>
+          <a
+            href="/#portfolio"
+            className="hover:bg-[#ff9a00] border-[1px] border-[#ff9a00] text-white place-self-start font-semibold rounded-full text-md md:text-md py-2 px-4 tracking-wide shadow-md"
+          >
+            MY WORK
+          </a>
+        </div>
       </div>
+      <div className="text-white hidden lg:block w-[100%] relative z-10">
+        <img
+          className="h-[800px] w-full object-cover overflow-visible z-10"
+          src={img}
+          alt="oussama ichid"
+        />
+        <div className="absolute top-32 left-20 w-72 h-72 rounded-full bg-[#ff9a00] blur-3xl -z-10"></div>
+      </div>
+      <div className="absolute -top-10 -right-20 w-52 h-52 rounded-full bg-[#ff9a0080] blur-3xl z-10"></div>
     </div>
   );
 };
